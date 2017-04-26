903b00b845e68fc2d595b959b67dea785990ea09
tjz3497 <tjz3497@rit.edu>
2017-04-25 22:54:27 -0400

    Basic TZIMMNN fetching done
    Call google maps timezone api on the given lat/lon

--- TZA.py
+++ TZA.py
@@ -1,15 +1,29 @@
+#IMPORT BLOCK
 import requests
 import re
+import time
 from keys_local import *
 
+#VARIABLE BLOCK for ip to lat
 IPINFO_START = "http://ipinfo.io/"
 IPINFO_END = "/loc"
+
+#get ip lat/long
 json = requests.get(IPINFO_START+'8.8.8.8'+IPINFO_END)
 for i in json:
 	loc = i.strip()
 loc = str(loc)
-loc = re.sub("[b']", '', loc)
-latlon = loc.split(",")
-print(latlon[0])
-print(latlon[1], end='')
+latlon = re.sub("[b']", '', loc)
+
+#get timestamp for map timezone api call
+timestamp = time.time()
+timestamp = str(timestamp).split(".")
+timestamp = timestamp[0]
+
+#VARIABLE BLOCK for map timezone api
+TZ_CALL = "https://maps.googleapis.com/maps/api/timezone/json?location="+latlon+"&timestamp="+timestamp+"&key="+MAPS_TZ_API
+tz_out = requests.get(TZ_CALL)
+for i in tz_out:
+	print(i)
+
 exit()
