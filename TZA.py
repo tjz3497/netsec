f39c1525837eb39857d8c2608b1628a69c00369f
tjz3497 <tjz3497@rit.edu>
2017-04-26 00:28:26 -0400

    timezones are weird
    trying to reconcile the offsets given by google with actual times for
    humans

--- TZA.py
+++ TZA.py
@@ -9,6 +9,38 @@ import json
 IPINFO_START = "http://ipinfo.io/"
 IPINFO_END = "/loc"
 
+#get timestamp for map timezone api call
+timestamp = time.time()
+timestamp = str(timestamp).split(".")
+timestamp = timestamp[0]
+
+#######LOCAL######################
+#get local info
+json = requests.get(IPINFO_START + "loc")
+for i in json:
+	loc = i.strip()
+loc = str(loc)
+latlon = re.sub("[b']", '', loc)
+
+#VARIABLE BLOCK for map timezone api
+TZ_CALL = "https://maps.googleapis.com/maps/api/timezone/json?location="+latlon+"&timestamp="+timestamp+"&key="+MAPS_TZ_API
+
+#get timezone from the goog
+tz_out = requests.get(TZ_CALL)
+data = tz_out.json()
+
+#combine raw offset with DST
+raw = data["rawOffset"]
+dst = data["dstOffset"]
+#if raw < 0:
+#	localTot = raw-dst
+#else:
+localTot = raw+dst
+print(localTot)
+print("Local Timezone is: "+data["timeZoneName"])
+#######END LOCAL######################
+
+#######DEST######################
 #get ip lat/long
 json = requests.get(IPINFO_START+'8.8.8.8'+IPINFO_END)
 for i in json:
@@ -16,14 +48,22 @@ for i in json:
 loc = str(loc)
 latlon = re.sub("[b']", '', loc)
 
-#get timestamp for map timezone api call
-timestamp = time.time()
-timestamp = str(timestamp).split(".")
-timestamp = timestamp[0]
-
 #VARIABLE BLOCK for map timezone api
 TZ_CALL = "https://maps.googleapis.com/maps/api/timezone/json?location="+latlon+"&timestamp="+timestamp+"&key="+MAPS_TZ_API
+
+#get timezone from the goog
 tz_out = requests.get(TZ_CALL)
 data = tz_out.json()
-print(data["rawOffset"])
+
+#combine raw offset with DST
+raw = data["rawOffset"]
+dst = data["dstOffset"]
+#if raw < 0:
+#	destTot = raw-dst
+#else:
+destTot = raw+dst
+print(destTot)
+print("Destination Timezone is: "+data["timeZoneName"])
+#######END DEST######################
+
 exit()
