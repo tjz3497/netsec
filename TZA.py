ed29c8a7e0712b6298b12ea8403042766fc2eff7
tjz3497 <tjz3497@rit.edu>
2017-04-27 14:40:24 -0400

    made functions
    pcap cutter works better and the code is all prettier

--- TZA.py
+++ TZA.py
@@ -6,75 +6,76 @@ import json
 
 from keys_local import *
 from pcap_in import *
+
+
+#VARIABLE BLOCK for ip to lat
+IPINFO_START = "http://ipinfo.io/"
+IPINFO_SL = "/loc"
+IPINFO_END = "loc"
+
+#VARIABLE BLOCK for timezone api call
+TZ_START = "https://maps.googleapis.com/maps/api/timezone/json?location="
+TZ_STAMP = "&timestamp="
+TZ_KEY = "&key="
+
 def the_goods():
+
 	#GET THE PCAP STUFF
 	PCAP_DICT = cutter()
 	#for i, j in PCAP_DICT.items():
 	#	print(i + " , ", end='')
-	#	print(time.ctime(j))
+	#	print(j)
 
-	#VARIABLE BLOCK for ip to lat
-	IPINFO_START = "http://ipinfo.io/"
-	IPINFO_END = "/loc"
-
-	#get timestamp for map timezone api call
+	#get local timestamp for maps timezone api call
 	timestamp = time.time()
 	timestamp = str(timestamp).split(".")
 	timestamp = timestamp[0]
+	#make it an int for math later
 	timeInt = int(timestamp)
-
-	#TEST
-	print(time.ctime(timeInt))
+	#print(time.ctime(timeInt))
 	#account for local  time offset of 4 hours
 	timeInt = timeInt + (3600 * 4)
 
-	#######LOCAL######################
-	#get local info
-	json = requests.get(IPINFO_START + "loc")
-	for i in json:
-		loc = i.strip()
-	loc = str(loc)
-	latlon = re.sub("[b']", '', loc)
-
-	#VARIABLE BLOCK for map timezone api
-	TZ_CALL = "https://maps.googleapis.com/maps/api/timezone/json?location="+latlon+"&timestamp="+timestamp+"&key="+MAPS_TZ_API
-
-	#get timezone from the goog
-	tz_out = requests.get(TZ_CALL)
-	data = tz_out.json()
-
-	#combine raw offset with DST
-	raw = data["rawOffset"]
-	dst = data["dstOffset"]
-
-	localTot = raw+dst+timeInt
-	print(time.ctime(localTot))
-	print("Local Timezone is: "+data["timeZoneName"])
-	#######END LOCAL######################
-
-	#######DEST######################
-	#get ip lat/long
-	json = requests.get(IPINFO_START+'8.8.8.8'+IPINFO_END)
-	for i in json:
-		loc = i.strip()
-	loc = str(loc)
-	latlon = re.sub("[b']", '', loc)
-
-	#VARIABLE BLOCK for map timezone api
-	TZ_CALL = "https://maps.googleapis.com/maps/api/timezone/json?location="+latlon+"&timestamp="+timestamp+"&key="+MAPS_TZ_API
-
-	#get timezone from the goog
-	tz_out = requests.get(TZ_CALL)
-	data = tz_out.json()
-
-	#combine raw offset with DST
-	raw = data["rawOffset"]
-	dst = data["dstOffset"]
-
-	destTot = raw+dst+timeInt
-	print(time.ctime(destTot))
-	print("Destination Timezone is: "+data["timeZoneName"])
-	#######END DEST######################
-
+	#localTot = get_time_info(islocal = 1, ip = 0, stamp = timestamp, tint = timeInt)
+	#print(time.ctime(localTot))
+	tester = 0
+	for i, j in PCAP_DICT.items():
+		if tester == 1:
+			ipper = i
+		tester = tester + 1
+	print(ipper)
+	destTot = get_time_info(islocal = 0, ip = ipper, stamp = timestamp, tint = timeInt)
+	print(time.ctime(destTot[2]))
 	exit()
+
+def get_time_info(islocal, ip, stamp, tint):
+	req = ""
+	#if its not local we need to supply an IP address in the request
+	#noIP
+	if islocal == 1:
+		req = IPINFO_START + IPINFO_END
+	#IP
+	else:
+		req = IPINFO_START + ip + IPINFO_SL
+	#send the request for the latitude and longitude
+	json = requests.get(req)
+	#make the answer pretty
+	for i in json:
+		loc = i.strip()
+	loc = str(loc)
+	latlon = re.sub("[b']",'',loc)
+
+	#get the timezone for the given latitude and longitude
+	TZ_CALL = TZ_START+latlon+TZ_STAMP+stamp+TZ_KEY+MAPS_TZ_API
+	tz_out = requests.get(TZ_CALL)
+	data = tz_out.json()
+
+	#get the raw offset and DST offset values
+	raw = data["rawOffset"]
+	dst = data["dstOffset"]
+	#add em up for the actual time
+	timeTot = raw+dst+tint
+	results = (raw, dst, timeTot)
+	return results
+
 the_goods()
--- pcap_in.py
+++ pcap_in.py
@@ -1,18 +1,20 @@
 from scapy.all import *
 import time
+import ipaddress
 
 def cutter():
-#pkts = rdpcap("SkypeIRC.cap")
+	#pkts = rdpcap("SkypeIRC.cap")
 	pkts = rdpcap("data.pcap")
 
-	bad = ['0.0.0.0', '??']
 	cut = {}
 	for pkt in pkts:
 		key = (pkt.sprintf("%IP.src%")).strip()
-		for k in bad:
-			if not (key == k):
-				cut[key] = pkt.time
-	#	temp = pkt.sprintf("%IP.src%,%IP.dst%,")
-	#	cut = cut + temp
+
+		addr = ipaddress.ip_address(key)
+		if not (addr.is_private):
+			time = str(pkt.time).split(".")
+			time = time[0]
+			#cut[key] = int(time)
+			cut[key] = time
 
 	return cut;
